37a5b9e4b24bb71e59f4bfa867fd316493bb2d20
McLeopold <mcleopold@gmail.com>
2017-02-17 23:31:51 -0800

    Add graph ps script

---
+++ graph.ps1
@@ -0,0 +1,14 @@
+﻿function git_graph {
+    $graph ="digraph {`n"
+    $commits = git rev-list --all
+    foreach ($commit in $commits) {
+        $parents = (git rev-list --parents -n 1 $commit) -split " "
+        foreach ($parent in $parents[1..10]) {
+            $graph += "C$($parent.Substring(0,6)) -> C$($commit.Substring(0,6))`n"
+        }
+    }
+    $graph += "}"
+    return $graph
+}
+
+Write-Output $(git_graph)
